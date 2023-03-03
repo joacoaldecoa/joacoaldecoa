6b1a538d61a5da61e3784033b665fa16508b724b
Cristian Cudos <crisguille@gmail.com>
2023-03-02 23:27:25 -0300

    Added Contact
    also  fix phones dynamic

---
+++ docs/_app/immutable/chunks/0-f8accc25.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-79fee81d.js";import{default as t}from"../components/pages/_layout.svelte-ae9f893a.js";export{t as component,r as universal};
---
+++ docs/_app/immutable/chunks/4-848c7b84.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-ed77219c.js";import{default as t}from"../components/pages/contact/_page.svelte-2825f457.js";export{t as component,r as universal};
